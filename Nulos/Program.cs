ac19ddecf0339ba5992935abc87266f09342fb14
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-24 07:45:48 -0700

    Agregar archivos de proyecto.

---
+++ Nulos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro miCarro = new Carro();
+            miCarro.Marca = "tsuru";
+            miCarro.kilometraje = 100000;
+            Console.WriteLine("Marca: " + miCarro.Marca);
+            Console.WriteLine("Km: " + miCarro.kilometraje);
+            if (miCarro.DueñoCarro ! = null)
+                {
+                Console.WriteLine("Dueño: " + miCarro.DueñoCarro.Nombre);
+            }
+            else
+            {
+                Console.WriteLine("No se encontro dieño");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
